b558ede3d75c1f80c80781ce34175d841be6b5fd
B1o0dy_Trey <a1306575900@gmail.com>
2021-10-29 15:57:06 +0800

    create project

---
+++ src/main/java/cn/tjpu/pilihub/PilihubApplication.java
@@ -0,0 +1,13 @@
+package cn.tjpu.pilihub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilihubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PilihubApplication.class, args);
+    }
+
+}
